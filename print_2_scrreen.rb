1fd86d7bca33b3279209ff93852d666bcae2b6b7
michaelhanna5 <peytommanningz@gmail.com>
2017-06-23 12:10:31 -0400

    even replace

--- print_2_scrreen.rb
+++ print_2_scrreen.rb
@@ -1,14 +1,10 @@
- num = 0
+num = 0
 
- # 1.upto(100) { |x| puts x }
- 100.times do 
- num += 1
- 	if num % 2 == 0
- 		puts "even"
- 	elsif num % 3 == 0
- 		puts "mined"
-    else
- 		puts num 
-	end
+100.times do
+	num += 1
+		if num % 2 == 0
+			puts "mined"
+		else
+			puts num
+		end
 end
-
